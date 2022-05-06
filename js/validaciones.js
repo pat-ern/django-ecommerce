6231577fc2c1739c2980ea39cdabdd2957c92603
noemimerari123 <no.gonzalez@duocuc.cl>
2022-05-05 21:56:57 -0400

    validaciones y mejoras formulario donaciones

--- js/validaciones.js
+++ js/validaciones.js
@@ -4,27 +4,37 @@ const inputs = document.querySelectorAll('#formulario input')
 
 const expresiones = {
     nombre: /^[a-zA-ZÁ-ÿ\s]{2,40}$/, //letras y espacios
-    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\[a-zA-Z0-9-.]+$/, //letras y espacios puede llevar acentos
-    telefono: /^\d{8,11}$/ // 8 a 11 digitos
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, //letras y espacios puede llevar acentos
+    telefono: /^\d{9,11}$/ // 9 a 11 digitos
 };
+
+const campos = {
+    nombre: false,
+    correo: false,
+    telefono: false
+}
 
 const validarFormulario = (e) => {
     switch (e.target.name){
         case "nombre":
-            if(expresiones.nombre.test(e.target.value)){
-                console.log("valor valido");
-                document.getElementById("grupo_nombre").classList.remove("error-activo");
-                document.getElementById("grupo_nombre").classList.remove("error-icon");
-            }else{
-                console.log("valor invalido");
-                document.getElementById("grupo_nombre").classList.add("error-activo");
-                document.getElementById("grupo_nombre").classList.add("error-icon");
-            }
+            validarCampo(expresiones.nombre, e.target, e.target.name);
         break;
         case "email":
+            validarCampo(expresiones.correo, e.target, e.target.name);
         break;
         case "celular":
+            validarCampo(expresiones.telefono, e.target, e.target.name);
         break;
+    }
+}
+
+const validarCampo =(expresion, input, campo) => {
+    if(expresion.test(input.value)){
+        document.getElementById(`grupo_${campo}`).classList.remove("formulario_grupo_incorrecto");
+        campos[campo] = true;
+    }else{
+        document.getElementById(`grupo_${campo}`).classList.add("formulario_grupo_incorrecto");
+        campos[campo] = false;
     }
 }
 
@@ -34,10 +44,19 @@ inputs.forEach((input) => {
 });
 
 formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-});
+    e.preventDefault(); //  Previene que se envie
 
 
-$("#enviar").click(function() {
-    alert("Enviado");
+    console.log(campos.nombre);
+    
+    console.log(campos.correo); // Es falso incluso cuando esta correcto, hay que mejorarlo o nunca entrará al if 
+    
+    console.log(campos.telefono);
+    
+
+
+    if(campos.nombre && campos.correo && campos.telefono){
+        formulario.reset();
+        alert("Se envio correctamente");
+    }
 });
