223c81fd151685088958348c29d7f6a8d3e3129a
noemimerari123 <no.gonzalez@duocuc.cl>
2022-05-07 20:03:09 -0400

    validaciones 2 formularios

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,19 +1,23 @@
-/**/
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input')
 
 const expresiones = {
-    nombre: /^[a-zA-ZÁ-ÿ\s]{2,40}$/, //letras y espacios
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, //letras y espacios puede llevar acentos
-    celular: /^\d{9,11}$/ // 9 a 11 digitos
+    nombre: /^[a-zA-ZÁ-ÿ\s]{2,40}$/, //  letras y espacios
+    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, //  formato correo
+    celular: /^\d{9,11}$/, // 9 a 11 digitos
+    frases: /^[a-zA-ZÁ-ÿ\s]{4,400}$/, //  letras y espacios 
 };
 
 const donacion = document.getElementById("donativo");
+const asunto = document.getElementById("asunto");
+const escribenos = document.getElementById("escribenos");
 
 const campos = {
     nombre: false,
     email: false,
-    celular: false
+    celular: false,
+    asunto: false,
+    escribenos: false
 }
 
 const validarFormulario = (e) => {
@@ -29,9 +33,9 @@ const validarFormulario = (e) => {
         break;
         case "donativo":
             if(donacion.value>=1000){
-                document.getElementById("donativo").classList.remove("borde_error_simple");
+                document.getElementById(e.target.name).classList.remove("borde_error_simple");
             }else{
-                document.getElementById("donativo").classList.add("borde_error_simple");
+                document.getElementById(e.target.name).classList.add("borde_error_simple");
             }
         break;
     }
@@ -52,12 +56,36 @@ inputs.forEach((input) => {
     input.addEventListener('blur', validarFormulario);
 });
 
+const validacionVacio = (e) => {
+    switch (e.target.name){
+        case "asunto":
+            if(e.target.value>=1 && e.target.value<=4){
+                document.getElementById('asunto').classList.remove("borde_error_simple");
+                campos[e.target.name] = true;
+            }else{
+                document.getElementById('asunto').classList.add("borde_error_simple");
+                campos[e.target.name] = false;
+            }
+        break;
+        case "escribenos":
+            if(expresiones.frases.test(e.target.value)){
+                document.getElementById("escribenos").classList.remove("borde_error_simple");
+            }else{
+                document.getElementById("escribenos").classList.add("borde_error_simple");
+            }
+        break;
+    }
+}
+
+asunto.addEventListener('change', validacionVacio);
+asunto.addEventListener('blur', validacionVacio);
+escribenos.addEventListener('blur', validacionVacio);
+escribenos.addEventListener('keyup', validacionVacio);
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault(); //  Previene que se envie
 
-
-    if(campos.nombre && campos.email && campos.celular && donacion.value>=1000){
+    if((campos.nombre && campos.email && campos.celular && donacion.value>=1000)||(campos.email && campos.asunto)){
         formulario.reset();
         alert("Se envio correctamente");
     }
