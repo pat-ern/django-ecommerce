150fe96796e015c33150fbf990de252a54a7972c
pat-ern <pat.villarroel@duocuc.cl>
2022-06-05 23:38:51 -0400

    arreglos

---
+++ laChacrita/app/static/app/js/conocenos.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#mision-vision").hide();
+  
+    $(".btn-mision").click(function(){
+      $("#mision-vision").slideToggle(1000);
+      $("#nuestra-historia").slideToggle(1000);
+    });
+  
+    $(".btn-historia").click(function(){
+      $("#nuestra-historia").slideToggle(1000);
+      $("#mision-vision").slideToggle(1000);
+    });
+  });
---
+++ laChacrita/app/static/app/js/producto.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#div-comentarios").show();
+
+    $("#btn-toggle").click(function(){
+        $("#div-comentarios").slideToggle(800);
+      });
+
+});
